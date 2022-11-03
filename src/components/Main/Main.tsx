5990f153f473ab059477cfe6df1fa178c6036d5b
boevoivertolet <bjha78rus@yandex.ru>
2022-11-03 14:34:00 +0300

    create common/ styles container

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Main.module.css'
+import styleContainer from '../../common/styles/Container.module.css'
 
 function Main() {
     return (
         <div className={styles.main}>
-            <div className={styles.container}>
+            <div className={styleContainer.container}>
                 <div className={styles.mainText}>
                     <span>HI there</span>
                     <h1>I am Alexander Beginin</h1>
