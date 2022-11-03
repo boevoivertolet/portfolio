9885b29e717da5fffe96a84c7168c17f815a9efb
boevoivertolet <bjha78rus@yandex.ru>
2022-11-03 14:29:05 +0300

    css for Main

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import Main from './components/Main/Main';
 function App() {
     return (
         <div className="App">
-           <Header/>
+            <Header/>
             <Main/>
+
         </div>
     );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,7 +4,16 @@ import styles from './Main.module.css'
 function Main() {
     return (
         <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.mainText}>
+                    <span>HI there</span>
+                    <h1>I am Alexander Beginin</h1>
+                    <p>Frontend Developer.</p>
+                </div>
+                <div className={styles.mainPhoto}>
 
+                </div>
+            </div>
         </div>
     );
 }
