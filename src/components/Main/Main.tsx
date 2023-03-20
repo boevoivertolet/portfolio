3eb02f5f7fdb19aeafaaaf488311dbf3c32f4eeb
boevoivertolet <bjha78rus@yandex.ru>
2023-03-20 17:19:07 +0300

    color for work

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,12 +5,12 @@ import styleContainer from '../../common/styles/Container.module.css'
 
 function Main() {
     return (
-        <div className={styles.main}>
-            <div className={`${styles.mainContainer} ${styleContainer.container}`}>
-                <div className={styles.mainText}>
-                    <span>HI there</span>
-                    <h1>I am Alexander Beginin</h1>
-                    <p>Frontend Developer.</p>
+        <div className = {styles.main}>
+            <div className = {`${styles.mainContainer} ${styleContainer.container}`}>
+                <div className = {styles.mainText}>
+                    <h1>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius mollitia nemo quos soluta? Animi consequuntur id itaque laboriosam, magnam minus optio quia quibusdam reiciendis repellat? Aspernatur fugit pariatur quaerat tempora vel. Commodi cumque eos fugit id itaque, minima obcaecati, quaerat, quibusdam quidem quis ratione recusandae rem sed sequi soluta! Accusantium asperiores dignissimos dolor minus modi natus nihil nulla quod. Iusto, natus, quia? Consectetur dolores ipsum molestiae numquam tenetur. Architecto commodi error facere facilis impedit maxime molestiae nesciunt perspiciatis repellendus sit. Ducimus labore non odit temporibus. Aperiam cumque eaque et explicabo laboriosam modi molestiae, nisi, pariatur quibusdam sapiente sit voluptatum?
+                    </h1>
                 </div>
 
             </div>
