80aedaeb06d7f12feecdd13114fc8fdaeda32274
boevoivertolet <bjha78rus@yandex.ru>
2023-03-20 15:35:37 +0300

    fix flex left right

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Skills from './components/Skills/Skills';
 import {MyWorks} from './components/MyWorks/MyWorks';
 import {Remote} from './components/Remote/Remote';
 import {Contacts} from './components/Contacts/Contacts';
 import {Footer} from './components/Footer/Footer';
-import styles from "./components/Main/Main.module.css";
-import MyPhoto from './assetes/userPhoto.jpg'
+import {PhotoCard} from "./PhotoCard/PhotoCard";
 
 
 function App() {
@@ -16,17 +14,7 @@ function App() {
     return (
         <div className = "App">
             <div className = {'left'}>
-
-
-                <div className = {styles.mainPhoto}>
-                    <h2 className={styles.myLogo}>Alexander <span>Frontend developer</span></h2>
-                    <img className={styles.myPhoto}  src = {MyPhoto} alt = "#" />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                                                       aperiam assumenda consequatur, dignissimos dolore dolorem hic
-                                                       illo ipsam iusto laborum laudantium nisi omnis quam repudiandae
-                                                       suscipit totam ullam veniam voluptas.</p>
-                    <Header />
-                </div>
+                <PhotoCard />
             </div>
             <div className = {'right'}>
                 <Main />
@@ -41,7 +29,5 @@ function App() {
 }
 
 export default App;
-
-
 
 
---
+++ src/PhotoCard/PhotoCard.tsx
@@ -0,0 +1,16 @@
+import styles from "../components/Main/Main.module.css";
+import MyPhoto from "../assetes/userPhoto.jpg";
+import Header from "../components/Header/Header";
+import React from "react";
+
+export const PhotoCard = () => {
+    return (
+        <div className = {styles.mainPhoto}>
+            <h2 className = {styles.myLogo}>Alexander <span>Frontend developer</span></h2>
+            <img className = {styles.myPhoto} src = {MyPhoto} alt = "#" />
+            <p>bjha78rus@yandex.ru</p>
+            <p>Base in Saint-Petersburg</p>
+            <Header />
+        </div>
+    )
+}
