fddf0440af85c0f66e4699fcc9d835bb1caf42d2
boevoivertolet <bjha78rus@yandex.ru>
2023-03-20 17:52:21 +0300

    fix css

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
             <div className = {`${styles.mainContainer} ${styleContainer.container}`}>
                 <div className = {styles.mainText}>
                     <h1>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius mollitia nemo quos soluta? Animi consequuntur id itaque laboriosam, magnam minus optio quia quibusdam reiciendis repellat? Aspernatur fugit pariatur quaerat tempora vel. Commodi cumque eos fugit id itaque, minima obcaecati, quaerat, quibusdam quidem quis ratione recusandae rem sed sequi soluta! Accusantium asperiores dignissimos dolor minus modi natus nihil nulla quod. Iusto, natus, quia? Consectetur dolores ipsum molestiae numquam tenetur. Architecto commodi error facere facilis impedit maxime molestiae nesciunt perspiciatis repellendus sit. Ducimus labore non odit temporibus. Aperiam cumque eaque et explicabo laboriosam modi molestiae, nisi, pariatur quibusdam sapiente sit voluptatum?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, explicabo.
                     </h1>
                 </div>
 
