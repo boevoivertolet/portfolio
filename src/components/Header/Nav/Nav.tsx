cd4a9c600bf2f3ea222a5e98a0a79a9cd18ce354
boevoivertolet <bjha78rus@yandex.ru>
2023-03-20 16:24:30 +0300

    add icons for PhotoCard

--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -3,11 +3,19 @@ import styles from './Nav.module.css'
 
 function Nav() {
     return (
-        <div className={styles.nav}>
-            <a href="">Main</a>
-            <a href="">Skills</a>
-            <a href="">Projects</a>
-            <a href="">Contacts</a>
+        <div className = {styles.nav}>
+            <a href = "">
+                <div><i className = "fa-solid fa-house"></i></div>
+            </a>
+            <a href = "">
+                <div><i className = "fa-brands fa-react"></i></div>
+            </a>
+            <a href = "">
+                <div><i className = "fa-solid fa-diagram-project"></i></div>
+            </a>
+            <a href = "">
+                <div><i className = "fa-sharp fa-solid fa-address-card"></i></div>
+            </a>
         </div>
     );
 }
