7a626a19827a0686e45f84277c6d48f9c0c2fa3e
boevoivertolet <bjha78rus@yandex.ru>
2023-03-27 14:47:42 +0300

    refactor Contact to scss

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Contacts.module.css';
+import styles from './Contacts.module.scss';
 import styleContainer from '../../common/styles/Container.module.scss';
 import {Title} from "../../common/components/Title";
 
@@ -15,21 +15,23 @@ export const Contacts = () => {
                         <div className = {styles.NameEmail}>
                             <div className = {styles.inputGroup}>
                                 <label>Ф.И.О<sup>*</sup></label>
-                                <input type = "text" name = "full-name" id = "full-name"
+                                <input className = {styles.formInput} type = "text" name = "full-name" id = "full-name"
                                        placeholder = "Ваше полное имя" />
                             </div>
                             <div className = {styles.inputGroup}>
                                 <label>Email<sup>*</sup></label>
-                                <input type = "text" name = "email" id = "email" placeholder = "Ваш почтовый адрес" />
+                                <input className = {styles.formInput} type = "text" name = "email" id = "email"
+                                       placeholder = "Ваш почтовый адрес" />
                             </div>
                         </div>
                         <div className = {styles.inputPhone}>
                             <label>Телефон</label>
-                            <input type = "text" name = "phone" id = "phone" placeholder = "Ваш номер телефона" />
+                            <input className = {styles.formInput} type = "text" name = "phone" id = "phone"
+                                   placeholder = "Ваш номер телефона" />
                         </div>
                         <div className = {styles.textareaGroup}>
-                            <label>Сообщение</label>
-                            <textarea name = "message" id = "message"
+                            <label className = {styles.textareaGroupLabel}>Сообщение</label>
+                            <textarea className = {styles.formTextarea} name = "message" id = "message"
                                       placeholder = "Напишите своё сообщение для меня здесь ..."></textarea>
                         </div>
                     </form>
