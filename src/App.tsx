82ce26cd5fd3d25464b6c906bf7df11bd8e4c851
boevoivertolet <bjha78rus@yandex.ru>
2023-05-04 13:28:03 +0300

    create Particle

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,31 @@ import {Contacts} from './components/Contacts/Contacts'
 import {Footer} from './components/Footer/Footer'
 import {PhotoCard} from './components/PhotoCard/PhotoCard'
 import {UpButton} from "./components/UpButton/UpButton.";
+import Particle from "./common/components/Particle";
+
 
 function App() {
     return (
-        <div className = 'App'>
-            <div className = {'left'}>
-                <article id = 'top'></article>
-                <PhotoCard />
-            </div>
-            <div className = {'right'}>
-                <article id = 'main'></article>
-                <Main />
-                <article id = 'skills'></article>
-                <Skills />
-                <article id = 'works'></article>
-                <MyWorks />
-                <article id = 'contacts'></article>
-                <Contacts />
-                <UpButton />
-                <Footer />
-            </div>
-
-        </div>
+        // <div className = 'App'>
+        //     <div className = {'left'}>
+        //         <article id = 'top'></article>
+        //         <PhotoCard />
+        //     </div>
+        //     <div className = {'right'}>
+        //         <article id = 'main'></article>
+        //         <Main />
+        //         <article id = 'skills'></article>
+        //         <Skills />
+        //         <article id = 'works'></article>
+        //         <MyWorks />
+        //         <article id = 'contacts'></article>
+        //         <Contacts />
+        //         <UpButton />
+        //         <Footer />
+        //     </div>
+        //
+        // </div>
+        <Particle />
     )
 }
 
