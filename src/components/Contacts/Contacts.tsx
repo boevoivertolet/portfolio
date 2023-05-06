5b118d0e6a78f07855d5534cf46273ee2376eae0
boevoivertolet <bjha78rus@yandex.ru>
2023-05-06 16:36:52 +0300

    try useForm

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Contacts.module.scss';
 import styleContainer from '../../common/styles/Container.module.scss';
 import {Title} from "../../common/components/Title";
 import {NameEmailPhone} from "./NameEmailPhone";
 import {TextareaGroup} from "./TextareaGroup";
 import {Fade} from "react-awesome-reveal";
+import {MyForm} from "../../common/components/Form/MyForm";
 
 
 export const Contacts = () => {
+
+
     return (
         <div className = {`${styles.contactsBlock} ${styleContainer.container}`}>
             <Title text = {'Мы можем поработать '} textSpan = {'вместе'} />
             <p>Со мной можно связаться по почте begininworkspace@gmail.com. Или заполнив форму.</p>
-            <Fade damping = {1} delay = {500} triggerOnce={true} className = {`${styleContainer.container} ${styles.contactsContainer}`}>
+            <Fade damping = {1} delay = {500} triggerOnce = {true}
+                  className = {`${styleContainer.container} ${styles.contactsContainer}`}>
                 <div className = {styles.content}>
-                    <form className = {styles.form}>
-                        <div className = {styles.formData}>
-                            <NameEmailPhone />
-                            <TextareaGroup />
-                        </div>
-                        <button type = {'submit'} className = {styles.button}>Отправить сообщение</button>
-                    </form>
+                    {/*<form className = {styles.form}>*/}
+                    {/*    <div className = {styles.formData}>*/}
+                    {/*        <NameEmailPhone />*/}
+                    {/*        <TextareaGroup />*/}
+                    {/*    </div>*/}
+                    {/*    <button type = {'submit'} className = {styles.button}>Отправить сообщение</button>*/}
+                    {/*</form>*/}
+                    <MyForm />
                 </div>
             </Fade>
 
--- src/components/Contacts/NameEmailPhone.tsx
+++ src/components/Contacts/NameEmailPhone.tsx
@@ -1,9 +1,8 @@
 import styles from "./Contacts.module.scss";
 import {InputGroup} from "./InputGroup";
-import React from "react";
+import React, {useState} from "react";
 
 export const NameEmailPhone = () => {
-
     return (
         <div className = {styles.nameEmailPhone}>
             <InputGroup type = {'text'} id = {"full-name"} name = {"full-name"} label = {'Ф.И.О'}
