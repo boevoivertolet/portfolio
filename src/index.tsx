8f3e4a7772206f0dc625904eeac039b0cc2f8fd5
boevoivertolet <bjha78rus@yandex.ru>
2024-07-11 14:25:58 +0300

    add hashorouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
 )
 
 reportWebVitals()
